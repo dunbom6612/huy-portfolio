33d207abba1b88e450dea9da1145ae104098dcaf
Huy Pham <pqhuy9795@gmail.com>
2021-07-23 23:47:51 +0700

    Complete project

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,46 +1,92 @@
 const wraper = document.querySelector(".cv");
 
 // light
-const linesPathLight = './lines.svg';
-const dotPaneLight = './dots-pane.svg'
-const triangleLight = './triangle.svg';
-const dotsCircleLight = './dots-circle.svg';
+const threeLinesPathLight = "./3-lines.svg";
+const linesPathLight = "./lines.svg";
+const dotPaneLight = "./dots-pane.svg";
+const triangleLight = "./triangle.svg";
+const dotsCircleLight = "./dots-circle.svg";
 // dark
-const linesPathDark = './lines-dark.svg';
-const dotPaneDark = './dots-pane-dark.svg'
-const triangleDark = './triangle-dark.svg';
-const dotsCircleDark = './dots-circle-dark.svg';
+const threeLinesPathDark = "./3-lines-dark.svg";
+const linesPathDark = "./lines-dark.svg";
+const dotPaneDark = "./dots-pane-dark.svg";
+const triangleDark = "./triangle-dark.svg";
+const dotsCircleDark = "./dots-circle-dark.svg";
 // icon sun moon
-const iconSun = './icon/sun.svg';
-const iconMoon = './icon/moon.svg';
+const iconSun = "./icon/sun.svg";
+const iconMoon = "./icon/moon.svg";
 
 
-document.querySelector(".cv-mode").addEventListener("click",(e) => {
-  // wraper.classList.add("dark");
-  const isDarkMode = localStorage.getItem("darkMode") === 'true'
-  console.log(isDarkMode);
-  if(isDarkMode){
-    document.querySelector(".effect-lines").setAttribute("src", linesPathLight);
-    document.querySelector(".effect-dots-pane").setAttribute("src", dotPaneLight);
-    document.querySelector(".effect-triangle").setAttribute("src", triangleLight);
-    document.querySelector(".effect-dots-circle").setAttribute("src", dotsCircleLight);
-    document.querySelector(".effect-dots-pane-2").setAttribute("src", dotsCircleLight);
-    document.querySelector("#dark-light-btn img").setAttribute("src",iconSun);
-    document.querySelector('.cv').classList.remove('dark-mode');
-  }else{
+
+const setDarkMode = (dark = true) => {
+  if (dark) {
+    //set to dark
+    document
+      .querySelector(".effect-3-lines")
+      .setAttribute("src", threeLinesPathDark);
     document.querySelector(".effect-lines").setAttribute("src", linesPathDark);
-    document.querySelector(".effect-dots-pane").setAttribute("src", dotPaneDark);
-    document.querySelector(".effect-triangle").setAttribute("src", triangleDark);
-    document.querySelector(".effect-dots-circle").setAttribute("src", dotsCircleDark);
-    document.querySelector(".effect-dots-pane-2").setAttribute("src", dotsCircleDark);
-    document.querySelector('.cv').classList.add('dark-mode');
-    document.querySelector("#dark-light-btn img").setAttribute("src",iconMoon);
+    document
+      .querySelector(".effect-dots-pane")
+      .setAttribute("src", dotPaneDark);
+    document
+      .querySelector(".effect-triangle")
+      .setAttribute("src", triangleDark);
+    document
+      .querySelector(".effect-dots-circle")
+      .setAttribute("src", dotsCircleDark);
+    document
+      .querySelector(".effect-dots-pane-2")
+      .setAttribute("src", dotPaneDark);
+    document.querySelector("#dark-light-btn img").setAttribute("src", iconMoon);
+    document.querySelector("#dark-light-btn span").textContent = "Dark";
+    document.querySelector(".cv").classList.add("dark-mode");
+  } else {
+    // set to light
+    document
+      .querySelector(".effect-3-lines")
+      .setAttribute("src", threeLinesPathLight);
+    document.querySelector(".effect-lines").setAttribute("src", linesPathLight);
+    document
+      .querySelector(".effect-dots-pane")
+      .setAttribute("src", dotPaneLight);
+    document
+      .querySelector(".effect-triangle")
+      .setAttribute("src", triangleLight);
+    document
+      .querySelector(".effect-dots-circle")
+      .setAttribute("src", dotsCircleLight);
+    document
+      .querySelector(".effect-dots-pane-2")
+      .setAttribute("src", dotPaneLight);
+    document.querySelector("#dark-light-btn img").setAttribute("src", iconSun);
+    document.querySelector("#dark-light-btn span").textContent = "Light";
+    document.querySelector(".cv").classList.remove("dark-mode");
+  }
+};
+
+// get from local storage and set mode
+const isDarkMode = localStorage.getItem("darkMode") === "true";
+if(isDarkMode){
+  setDarkMode(true);
+}else{
+  setDarkMode(false);
+}
+
+document.querySelector(".cv-mode").addEventListener("click", (e) => {
+  // wraper.classList.add("dark");
+  const isDarkMode = localStorage.getItem("darkMode") === "true";
+  console.log(isDarkMode);
+  if (isDarkMode) {
+    // set to light
+    setDarkMode(false);
+  } else {
+    //set to dark
+    setDarkMode(true);
   }
 
-  if(isDarkMode){
+  if (isDarkMode) {
     localStorage.setItem("darkMode", JSON.stringify(null));
-  }else{
+  } else {
     localStorage.setItem("darkMode", true);
   }
-
-})
+});
